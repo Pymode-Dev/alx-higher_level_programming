b07dbfc14976ec5eca81596e31f78664f67b2740
Pymode-Dev <pymode000@gmail.com>
2023-09-07 17:51:19 -0700

    2. How to make a script dynamic

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-import sys
-
-length = len(sys.argv) - 1
-
-if length > 0:
-    print("{} {}:".format(length, "arguments" if length > 1 else "argument"))
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
-else:
-    print("0 arguments.")
+if __name__ == "__main__":
+    import sys
+    length = len(sys.argv) - 1
+    if length > 0:
+        print("{} {}:".format(length, "arguments"))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
+    else:
+        print("0 arguments.")
