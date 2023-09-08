57a6611e296d27968368f11c2fef2fe807e91619
Pymode-Dev <pymode000@gmail.com>
2023-09-08 15:31:58 -0700

    1. Secure access to an element in a list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    result = None
+    if idx < 0:
+        result = None
+    elif idx > len(my_list):
+        result = None
+    else:
+        result = my_list[idx]
+    return result
