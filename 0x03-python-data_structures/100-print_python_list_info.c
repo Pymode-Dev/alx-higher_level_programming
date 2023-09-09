3fdfadbf4201916c318089a860773743a35e1815
Pymode-Dev <pymode000@gmail.com>
2023-09-08 23:30:21 -0700

    Interaction with python code and c

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <Python.h>
+
+/**
+ * print_python_list_info - prints hidden info about list operation
+ * @p: PyObject
+ *
+ * Return: void
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	long int size, i;
+	PyListObject *list;
+	PyObject *item;
+
+	size = Py_SIZE(p);
+	printf("[*] size of the python list = %ld\n", size);
+
+	list = (PyListObject *)p;
+	printf("[*] Allocated = %ld\n", list->allocated);
+
+	i = 0;
+	for (; i< size; i++)
+	{
+		item = PyList_GetItem(p, i);
+		printf("Element %ld: %s\n", i, PyTYPE(item)->tp_name);
+	}
+}
