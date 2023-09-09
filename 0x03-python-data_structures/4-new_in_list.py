19c76c93b7602f6220fcd7ed53f1b2fbd06333b0
Pymode-Dev <pymode000@gmail.com>
2023-09-08 17:49:54 -0700

    4. Replace in a copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    result = my_list[:]
+    if idx < 0:
+        pass
+    elif idx > len(my_list):
+        pass
+    else:
+        result[idx] = element
+    return result
