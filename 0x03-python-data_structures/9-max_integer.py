d274e0bffd7c56dff291480d4349b04c53d3c966
Pymode-Dev <pymode000@gmail.com>
2023-09-08 23:06:09 -0700

    Fix logic error

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list is None:
-        result = None
+    if len(my_list) == 0:
+        return None
     else:
         result = sorted(my_list, reverse=True)[0]
     return result
