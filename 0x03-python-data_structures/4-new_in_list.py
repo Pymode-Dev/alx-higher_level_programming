e63c9a1d9bfe18ac814ed288393d22a1fa67d1a7
Pymode-Dev <pymode000@gmail.com>
2023-09-08 22:54:23 -0700

    Fix logic error and formatting style

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     result = my_list[:]
     if idx < 0:
         pass
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         pass
     else:
         result[idx] = element
