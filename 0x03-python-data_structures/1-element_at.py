92d449db23b093ea9e7e8168808b803b7e156117
Pymode-Dev <pymode000@gmail.com>
2023-09-08 22:48:58 -0700

    Fix logic error

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     result = None
     if idx < 0:
         result = None
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         result = None
     else:
         result = my_list[idx]
