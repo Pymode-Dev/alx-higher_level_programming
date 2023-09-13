84153fd3eeaf498265083fce8c97505f7758c8b7
Pymode-Dev <pymode000@gmail.com>
2023-09-13 13:40:13 -0700

    4. Only difference

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
+    first_set = set_2.difference(set_1)
     new_set = set_1.difference(set_2)
-    return new_set
+    return new_set.union(first_set)
