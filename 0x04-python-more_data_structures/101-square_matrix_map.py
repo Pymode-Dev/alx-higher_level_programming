770fd703a493b006900e5567df8df477edaff9f8
Pymode-Dev <pymode000@gmail.com>
2023-09-14 15:15:54 -0700

    14. Square matrix map

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda element: list(map(lambda element: element ** 2, element)), matrix))
+    return list(map(
+        lambda element: list(map(
+            lambda element: element ** 2, element)), matrix))
