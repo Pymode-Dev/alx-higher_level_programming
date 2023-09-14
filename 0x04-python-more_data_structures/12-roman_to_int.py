e12980807c21089c4ceaff7151ae14b5a7de9151
Pymode-Dev <pymode000@gmail.com>
2023-09-14 14:37:52 -0700

    12. Roman to integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    sample = {'I': 1, 'V': 5, 'X': 10,
+            'L': 50, 'C': 100, 'D': 500,
+            'M': 1000
+            }
+    integer = 0
+    prev_int = 0
+
+    for string in reversed(roman_string):
+        current_int = sample[string]
+
+        if current_int > prev_int:
+            integer += current_int
+        elif current_int == prev_int:
+            integer += current_int
+        else:
+            integer -= current_int
+        prev_int = current_int
+
+    return integer
