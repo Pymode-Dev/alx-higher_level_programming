6305e4c332bd5a504d72bc4b564122edd529c42f
Pymode-Dev <pymode000@gmail.com>
2023-09-27 05:44:44 -0700

    4. List division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -6,13 +6,13 @@ def list_division(my_list_1, my_list_2, list_length):
     for i in range(list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
-        except TypeError:
+        except (TypeError):
             result = 0
             print("Wrong type")
-        except ZeroDivisionError:
+        except (ZeroDivisionError):
             result = 0
             print("division by 0")
-        except IndexError:
+        except (IndexError):
             result = 0
             print("out of range")
         finally:
