11bc80d4da5b9469bb5d7fdd08f78254bed60845
Pymode-Dev <pymode000@gmail.com>
2023-09-27 06:55:12 -0700

    1. Square size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A module with Square class and a private attribute of size
+"""
+
+class Square:
+    """
+    A class Square that defines a square
+    """
+    def __init__(self, size):
+        self._size = size
