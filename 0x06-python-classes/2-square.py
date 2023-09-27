d2e6e0480f9a9ee5df5ba5061023642c64bedffd
Pymode-Dev <pymode000@gmail.com>
2023-09-27 15:15:07 -0700

    2. Square and safe initialization

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,9 +9,8 @@ class Square:
     A class Square that defines a square
     """
     def __init__(self, size=0):
-        try:
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = int(size)
-        except TypeError:
-            print("size must be an integer")
+        if int(size) < 0:
+            raise ValueError("size must be >= 0")
+        elif (type(size) is not int):
+            raise TypeError("size must be an integer")
+        self.__size = size
