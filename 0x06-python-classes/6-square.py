44b63485f30c29f7be70331f2720dfe0420c6e2b
Pymode-Dev <pymode000@gmail.com>
2023-09-27 16:38:35 -0700

    6. Coordinates of a square

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+A Square class is defined.
+"""
+
+
+class Square:
+    """
+    class: Square
+    method: area, getter, setter, my_print, position
+    """
+    def __init__(self, size=0, position=(0, 0)):
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        elif int(size) < 0:
+            raise ValueError("size must be >= 0")
+        elif isinstance(position, tuple) is False:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        elif int(size) < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if isinstance(value, tuple) is False:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        return int(self.__size) ** 2
+
+    def my_print(self):
+        if int(self.__size) == 0:
+            print()
+        else:
+            for i in range(int(self.__size)):
+                print("{}{}".format('_' * self.__position[0], self.__size * '#'))
