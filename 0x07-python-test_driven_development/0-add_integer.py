297a84919dd6f8802490045ac73c898375c2abaa
Pymode-Dev <pymode000@gmail.com>
2023-10-03 18:16:04 -0700

    0. Integers addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+add integers
+"""
+def add_integer(a, b=98):
+    """
+    Add two integers.
+    """
+    if (isinstance(a, int) or isinstance(a, float)) is False:
+        raise TypeError("a must be an integer")
+    if (isinstance(b, int) or isinstance(b, float)) is False:
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
