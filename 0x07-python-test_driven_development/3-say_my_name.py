bf66c3357e851075725d5e0ee21a6ef178f35c38
Pymode-Dev <pymode000@gmail.com>
2023-10-04 17:36:00 -0700

    3. Say my name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+"""
+say_my_name - module
+"""
+
 def say_my_name(first_name, last_name=""):
+    """
+    Print the full name of a user
+    Args:
+        first_name: str
+        last_name: str
+    Return:
+        None
+    """
     if isinstance(first_name, str) is False:
         raise TypeError("first_name must be a string")
     if isinstance(last_name, str) is False:
