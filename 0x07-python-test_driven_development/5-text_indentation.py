23b1e870a3a1f088e1a7cd08bddfbf7a78ee57c4
Pymode-Dev <pymode000@gmail.com>
2023-10-04 17:49:51 -0700

    5. Indent text

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python3
+"""
+5-text_indentation - module
+"""
+
+
 def text_indentation(text):
-    for i in text:
-        if i in ".?:":
-            print(i, end="")
-            print("\n")
-        else:
-            print(i, end="")
+    """
+    text_indentation - indent text according to some punctuation symbols
+    Args:
+        text: str
+    Return:
+        None
+    """
+    if isinstance(text, str) is False:
+        raise TypeError("text must be a string")
+
+    count = 0
+
+    while count < len(text) and text[count] == " ":
+        count += 1
+
+    while count < len(text):
+        print(text[count], end="")
+        if text[count] == '\n' or text[count] in ".?:":
+            if text[count] in ".?:":
+                print('\n')
+            count += 1
+            while count < len(text) and text[count] == " ":
+                count += 1
+                continue
+        count += 1
