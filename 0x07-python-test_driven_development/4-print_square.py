2fb58983c4f8f6b45296f9b0b3c0aed86fb2ccf5
Pymode-Dev <pymode000@gmail.com>
2023-10-04 17:39:18 -0700

    4. print square

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+"""
+print_square - module
+"""
+
+
 def print_square(size):
+    """
+    print_square - print square according to the size
+    Args:
+        size: int
+    Return:
+        None
+    """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if int(size) < 0:
