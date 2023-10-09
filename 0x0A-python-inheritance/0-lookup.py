49e1e2c4d1b93628d3f47bf404bf12fd2959f894
Pymode-Dev <pymode000@gmail.com>
2023-10-09 16:23:58 -0700

    0. Lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""
+0-lookup.py
+"""
 
 def lookup(obj):
+    """
+    lookup - function that returns the list of available attributes
+    and methods of an object
+    Args:
+        obj: the object
+    Return: list
+    """
     return dir(obj)
