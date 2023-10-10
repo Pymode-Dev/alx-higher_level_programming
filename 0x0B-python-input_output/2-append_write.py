d74237f1e9557030d4f2cb4cdcc7c7ec3a988b46
Pymode-Dev <pymode000@gmail.com>
2023-10-10 14:41:43 -0700

    2. Append to a file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+1-write_file.py
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    write_file: write to file
+    Args:
+        filename: the file to write to
+        text: the text to write
+    Return:
+        word_count: int
+    """
+    word_count = 0
+
+    with open(filename, mode="a", encoding="utf-8") as file:
+        for word in text:
+            file.write(word)
+            word_count += 1
+    return word_count
