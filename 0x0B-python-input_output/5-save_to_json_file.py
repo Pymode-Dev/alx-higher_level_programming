5354f51e2141f3123732b332d41bb19a8759764d
Pymode-Dev <pymode000@gmail.com>
2023-10-10 15:10:33 -0700

    5. Save to json file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -14,5 +14,5 @@ def save_to_json_file(my_obj, filename):
     Return:
         None
     """
-    with open(filename, mode="w")  as file:
+    with open(filename, mode="w") as file:
         json.dump(my_obj, file)
