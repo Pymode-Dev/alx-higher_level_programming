76fc08b4dadf37315c2879666cd5f91e55d70cb4
Pymode-Dev <pymode000@gmail.com>
2023-10-09 20:02:00 -0700

    11. Square #2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+8-rectangle.py
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square
+    """
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
