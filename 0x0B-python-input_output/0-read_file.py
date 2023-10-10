c09b4bea63863f1d6d21c689041b48d7fb201ffa
Pymode-Dev <pymode000@gmail.com>
2023-10-10 14:21:25 -0700

    0. Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,13 @@ import sys
 
 
 def read_file(filename=""):
+    """
+    read_file: read file to stdout
+    Args:
+        filename: the filepath
+    Return:
+        None
+    """
     with open(filename, mode="r", encoding="utf-8") as file:
         for line in file:
             sys.stdout.write(line)
