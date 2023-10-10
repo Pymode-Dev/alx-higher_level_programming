3e1a677c5d08cb1c291ed4796935fc60792431df
Pymode-Dev <pymode000@gmail.com>
2023-10-09 17:34:34 -0700

    6. Improved Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+5-base_geometry.py
+"""
+
+
+class BaseGeometry:
+    """
+    Class: BaseGeometry
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
