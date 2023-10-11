534d394f4dd87db481fd4b385ba31993a5f01554
Pymode-Dev <pymode000@gmail.com>
2023-10-11 12:26:24 -0700

    7. Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -11,6 +11,6 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 try:
     arguments = load_from_json_file('add_item.json')
 except FileNotFoundError:
-        arguments = []
+    arguments = []
 arguments.extend(sys.argv[1:])
 save_to_json_file(arguments, 'add_item.json')
