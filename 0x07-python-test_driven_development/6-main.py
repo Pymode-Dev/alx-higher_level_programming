f7c28871b1701ac9430f5e0a0f617c4a90f06f2f
Oyewale Joseph <76213360+Pymode-Dev@users.noreply.github.com>
2024-01-27 09:21:46 +0100

    Delete 0x07-python-test_driven_development directory

--- 0x07-python-test_driven_development/6-main.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-max_integer = __import__('6-max_integer').max_integer
-
-print(max_integer([1, 2, 3, 4]))
-print(max_integer([1, 3, 4, 2]))
-print(max_integer([-4, -3, -2, -1]))
-print(max_integer([1, 3.4, 2, 3]))
-print(max_integer([1.5, 1.55, 2.4, 4.7, 8.9, 8.98]))
