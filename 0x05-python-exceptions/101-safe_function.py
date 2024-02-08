e0b21ee46ba9d9207c0fb992af0257b77d6b99e7
Pymode-Dev <pymode000@gmail.com>
2024-02-07 17:28:47 -0800

    8. Safe function.

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
+import sys
 
 def safe_function(fct, *args):
     try:
         result = fct(*args)
     except Exception as error:
-        print(f"Exception: {error}")
+        print(f"Exception: {error}", file=sys.stderr)
         return None
     else:
         return result
