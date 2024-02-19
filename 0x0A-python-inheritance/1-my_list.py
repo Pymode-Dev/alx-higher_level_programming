11e1e0208a4d04d20ba7f9b361c7603951feed31
Pymode-Dev <pymode000@gmail.com>
2024-02-19 15:02:34 -0800

    1. My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+A class that inherits from list
+"""
+
+
+class MyList(list):
+    """
+    class
+        MyList - inherits from list
+    Method:
+        print_sorted: print the list in a sorted way
+    """
+    pass
+    def print_sorted(self):
+        print(sorted(self))
