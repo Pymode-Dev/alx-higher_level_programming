a55c5d48ca922ec5b596d98c0055d0e78e6f42fe
Pymode-Dev <pymode000@gmail.com>
2024-02-19 15:23:52 -0800

    2. Exact same object

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+2-is_same_class.py
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    is_same_class - returns true if the object is exactly
+    an instance of the specified class.
+    Args:
+        obj: the class to check
+        a_class: the class to check against obj
+    Return:
+        True or False
+    """
+    return type(obj) == a_class
