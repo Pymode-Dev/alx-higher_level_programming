4f9046c9de4a9f2780716297b334f3555513ccd3
Pymode-Dev <pymode000@gmail.com>
2024-02-19 17:45:08 -0800

    10. Square #1

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""nherits from base class."""
+Rectangle = __import__('9-rectangle').BaseGeometry
+
+
+class Square(Rectangle):
+    """
+    class
+        Retangle - it inherits from base geometry
+    Args:
+        width, height
+    """
+    def __init__(self, size):
+        """Initialize a new rectangle using BaseGeometry"""
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """calclates the area of a rectangle"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
