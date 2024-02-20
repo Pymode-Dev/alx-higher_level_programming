9fc74c778ceaa9acee4427d2c2581712c66f4d7c
Pymode-Dev <pymode000@gmail.com>
2024-02-19 16:51:19 -0800

    6. Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""An empty clas BseGeometry."""
+
+
+class BaseGeometry:
+    """
+    class
+        BaseGeometry: an empty class
+    """
+    pass
+
+    def area(self):
+        """
+        area - it calculates the area of any geometry
+        """
+        raise Exception("area() is not implemented")
