403aef598cb793efe986462023c55cea1bbfd185
Pymode-Dev <pymode000@gmail.com>
2024-02-20 16:05:16 -0800

    5. Save Object to a file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Writes an object to a text file using JSON rep."""
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    save_to_json_file - writes an object to a text file using a
+    JSON representatin.
+    Args:
+        my_obj: the object to save
+        filename: the filename to save the onbject to
+    Return:
+        None
+    """
+    import json
+
+    with open(filename, mode="w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
