a1150b67c338ad632d56df5e1672b2a0d042e8ab
Pymode-Dev <pymode000@gmail.com>
2024-02-20 16:15:54 -0800

    6. Create object from a JSON file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Creates an object from a JSON file"""
+
+
+def load_from_json_file(filename):
+    """
+    load_from_json_file - a function that creates an object
+    from a JSON file.
+    Args:
+        filename: the filename to creates the object from
+    Return:
+        None
+    """
+    import json
+
+    with open(filename, mode="r", encoding="utf-8") as f:
+        json.load(f)
