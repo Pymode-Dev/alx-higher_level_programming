4b68142d2af38bf3ad1b1ece99b5a0280eec4b1e
Pymode-Dev <pymode000@gmail.com>
2024-02-20 16:40:10 -0800

    7. Load, add, save

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -14,5 +14,5 @@ def save_to_json_file(my_obj, filename):
     """
     import json
 
-    with open(filename, mode="w", encoding="utf-8") as f:
+    with open(filename, mode="w") as f:
         json.dump(my_obj, f)
