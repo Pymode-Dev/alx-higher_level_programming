13a167530130363fa30c84a5958e0cc541db6587
Pymode-Dev <pymode00@gmail.com>
2024-02-26 17:57:39 -0800

    Add readme file to models folder

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,9 +5,20 @@ Write the firstclass Base:
 
 
 class Base:
+    """
+    class:
+        Base - the base class for all class in this project
+    Method:
+        None
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        __init__ - initialize a new class
+        Args:
+            id: None
+        """
         if id is not None:
             self.id = id
         else:
